481f2decaef3dfa8bae3701e96c26758d5a2b4c7
italosantana <flaubert165@gmail.com>
2017-12-16 15:21:51 -0300

    Initial commit

---
+++ platforms/ios/build/ECommerceMobile.build/Debug-iphonesimulator/ECommerceMobile.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/ECommerceMobile/ECommerceMobile-Prefix.pch \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/main.m \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/TNSDebugging.h \
+  /Users/italosantana/Desktop/ECommerce.Mobile/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
